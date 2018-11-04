93448ba53591cf2567549bab913019e711f9e765
GozaRuu <kais.sghari@gmail.com>
2018-11-04 08:59:11 +0100

    added responsive layout to styles

--- src/components/NumberPool.js
+++ src/components/NumberPool.js
@@ -4,21 +4,23 @@ import range from "lodash.range";
 const NumberPool = ({ numberPool, onClick, usedNumbers }) => (
   <div className="col-md-12 shadow-lg rounded d-flex p-2 align-items-center justify-content-center">
     <div className="col-md-12 shadow-lg rounded d-flex p-2 align-items-center justify-content-center">
-      {range(1, 10).map(number => {
-        if (usedNumbers.indexOf(number) !== -1) {
-          return (
-            <span key={number} className="selected">
-              {number}
-            </span>
-          );
-        } else {
-          return (
-            <span key={number} onClick={() => onClick(number)}>
-              {number}
-            </span>
-          );
-        }
-      })}
+      <div>
+        {range(1, 10).map(number => {
+          if (usedNumbers.indexOf(number) !== -1) {
+            return (
+              <span key={number} className="selected">
+                {number}
+              </span>
+            );
+          } else {
+            return (
+              <span key={number} onClick={() => onClick(number)}>
+                {number}
+              </span>
+            );
+          }
+        })}
+      </div>
     </div>
   </div>
 );
